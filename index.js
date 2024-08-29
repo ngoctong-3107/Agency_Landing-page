e8bb7b32873d3e838a06791897e406b786603b97
Pearl <75606688+ngoctong-3107@users.noreply.github.com>
2024-08-29 11:26:38 +0700

    Develop (#7)
    * style advantage
    * style feature
    * refactor code (#1)
    * style porfolio (#2)
    * style blogs (#3)
    * Footer (#4)
    * footer
    * Testimonial (#5)
    * Responsive (#6)
    * responsive
    * make slider in features
    * add button to top

---
+++ index.js
@@ -0,0 +1,54 @@
+const icon_menu = document.querySelector(".icon_menu");
+const icon_close = document.querySelector(".icon_close");
+const header_menulist = document.querySelector(".header_menu-list");
+const menu_items = document.querySelectorAll(".header_menu-list > li");
+
+icon_menu.addEventListener("click", () => {
+  header_menulist.style.display = "block";
+  header_menulist.style.right = 0;
+});
+icon_close.addEventListener("click", () => {
+  header_menulist.style.right = "-50%";
+  header_menulist.style.display = "none";
+});
+menu_items.forEach((item) => {
+  item.addEventListener("click", () => {
+    header_menulist.style.right = "-50%";
+  });
+});
+
+const portfolio_sidebarItem = document.querySelectorAll(
+  ".portfolio_sidebar_list > li"
+);
+
+portfolio_sidebarItem.forEach((item) =>
+  item.addEventListener("click", function (e) {
+    portfolio_sidebarItem.forEach((m) => m.classList.remove("active"));
+    e.target.classList.add("active");
+  })
+);
+
+const dropdown_value = document.querySelector(".dropdown-value");
+const dropdown_list = document.querySelector(".dropdown-list");
+const dropdown_items = document.querySelectorAll(".dropdown-item");
+dropdown_items.forEach((item) => {
+  item.addEventListener("click", () => {
+    dropdown_value.innerHTML = item.innerHTML;
+  });
+});
+
+const btnTop = document.querySelector("#topBtn");
+window.onscroll = function () {
+  scrollFunc();
+};
+function scrollFunc() {
+  if (document.body.scrollTop > 600) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+btnTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
